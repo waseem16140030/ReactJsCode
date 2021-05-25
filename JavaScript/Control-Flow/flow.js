328f2d421126979a787f24e14dd1b347ac32496f
waseem16140030 <waseem16140030@gmail.com>
2021-05-25 18:27:41 +0500

    Updated File

--- JavaScript/Control-Flow/flow.js
+++ JavaScript/Control-Flow/flow.js
@@ -155,5 +155,190 @@ function maxNumber(num1, num2){
         return num2;
     }
 }
-console.log("max number is" + maxNumber(5,4));
+console.log("max number is" + maxNumber(5, 4));
 /***********************************Exercise:1 ends ****************************************** */
+
+
+/***********************************isLandscape Exercise ****************************************** */
+ function isLandscape(width, height){
+    
+       return width>height? true: false;
+ }
+console.log("The image is landscape?"+  isLandscape(30, 20))
+/***********************************isLandscape Exercise End ****************************************** */
+
+
+/***********************************FizzBuzz function ****************************************** */
+//divisible by 3 ->fizbuzz
+//divible by 5->buzz
+//divisible by 5 and 3 ->fizbuzz
+//not divisible by 3 or 5 return the same number
+//input on string->not a numbr
+
+function FizBuzz(input){
+     if(typeof(input) !== 'number'){
+         return 'Not a number';
+     }
+     else if(input%3==0 && input%5==0){
+        return 'FizzBuzz';
+    }
+     else if(input%3==0){
+         return 'Fizz';
+     }
+     else if(input%5==0){
+         return 'Fuzz';
+     }
+     
+     else{
+         return input;
+     }
+}
+console.log(FizBuzz(15));
+/***********************************FizzBuzz function end ****************************************** */
+
+
+
+/***********************************Check spped Limit function ********************************/
+function checkSpeed(speed){
+    const spedLimit=70;
+    const pointsPerKM=5;
+    if(speed<=spedLimit+pointsPerKM){
+        console.log('Ok');
+    }
+    else{
+        let points= Math.floor(((speed-spedLimit)/pointsPerKM));
+        if(points > 12){
+            console.log("License cancelled");
+        }
+        else{
+            console.log('points', points);
+        }
+    }
+}
+
+checkSpeed(100);
+/***********************************Check spped Limit function end ********************************/
+
+/***********************************odd and Even Number********************************/
+
+function showNumbers(limit){
+    for(let i=0; i<=limit; i++){
+        if(i%2==0){
+            console.log(i, "EVEN");
+        }
+        else if(i%2!==0){
+            console.log(i, "ODD");
+        }
+    }
+}
+showNumbers(5);
+
+/***********************************odd and Even Number end********************************/
+
+/***********************************Calculate Truthy********************************/
+let array = [0, undefined, null, false, '', true, 'mosh', 123, ];
+let count=0;
+function calculateTruthy(array){
+    for(let value of array){
+        if(value){
+            count++;
+        }
+    }
+    return count;
+}
+console.log('thrthy values in array are',calculateTruthy(array));
+/***********************************Calculate Truthy end********************************/
+
+/***********************************Show properties of object********************************/
+let student={
+Name:"waseem",
+Age:24,
+section: 'Gold'
+};
+
+function showProperties(student){
+    for(let props in student){
+        if(typeof(student[props])==='string'){
+            console.log(props, student[props]);
+        }
+    }
+}
+showProperties(student);
+/***********************************Show properties of object ends********************************/
+
+/***********************************sum of multiples of 3 and 5********************************/
+function sumofLimit(limit){
+    let sum=0;
+    for(let i=0; i<=limit; i++){
+        if(i%3==0 || i%5==0){
+            sum+=i;
+        }
+    }
+    return sum;
+}
+console.log('sum of multiples of 3 and 5 is', sumofLimit(10));
+/***********************************sum of multiples of 3 and 5 end********************************/
+
+
+/***********************************Calculate Grades********************************/
+const grades=[60, 75, 97, 80, 84];
+let sum=0;
+let avg=0;
+function calculateGrade(grades){
+    for(let key in grades){
+        sum=sum+grades[key];
+    }
+    avg= Math.floor(sum/5);
+    if(avg>0 && avg<=59){
+        console.log('F');
+    }
+    else if (avg >60 && avg<= 69){
+        console.log('D');
+    }
+    else if (avg >70 && avg <= 79){
+        console.log('C');
+    }
+    else if (avg>80 && avg<=89){
+        console.log("B");
+    }
+    else if(avg >90 && avg<= 100){
+        console.log('A');
+    }
+}
+ calculateGrade(grades);
+/***********************************Calculate Grades ends********************************/
+
+
+/***********************************showStar********************************************/
+function showStar(num){
+    for(let i=1; i<=num; i++){
+        let pat="";
+        for(let j=0; j< i; j++){
+         pat=pat+ '*';
+        }
+        console.log(pat);
+    }
+}
+showStar(10);
+/***********************************showStar********************************************/
+
+/***********************************showPrimes********************************************/
+
+function showPrimes(limit){
+    let isPrime=true;
+   for(let num=2; num<=limit; num++){
+       isPrime=true;
+       for (let fac=2; fac< num; fac++){
+           if(num%fac===0){
+            isPrime=false;
+            break;
+           }
+       }
+       if(isPrime){
+           console.log(num);
+       }
+   }
+}
+showPrimes(10);
+
+/***********************************showPrimes ends********************************************/
